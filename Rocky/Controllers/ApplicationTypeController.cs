e71853dab1a2bbd835d8328b33caa9538894b076
Bularu Lilian <aryanlilian@gmail.com>
2021-05-12 21:43:11 +0300

    implemented the Edit feature for the models

--- Rocky/Controllers/ApplicationTypeController.cs
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -31,6 +31,7 @@ namespace Rocky.Controllers
             return View();
         }
 
+        // POST ApplicationType/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(ApplicationType obj)
@@ -38,6 +39,35 @@ namespace Rocky.Controllers
             if (ModelState.IsValid)
             {
                 _db.ApplicationType.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET - ApplicationType/Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - ApplicationType/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -45,5 +45,34 @@ namespace Rocky.Controllers
             }
             return View(obj);
         }
+
+        // GET - Category/Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - Category/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
