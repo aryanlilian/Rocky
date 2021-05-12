9e78ed7753f57049cd18bef535fed0d6e8fb1858
Bularu Lilian <aryanlilian@gmail.com>
2021-05-12 19:14:07 +0300

    implemented & configured form inputs validation on the Client side

--- Rocky/Models/Category.cs
+++ Rocky/Models/Category.cs
@@ -8,9 +8,14 @@ namespace Rocky.Models
     {
         [Key]
         public int Id { get; set; }
+
         [DisplayName("Name")]
+        [Required]
         public string Name { get; set; }
+
         [DisplayName("Display Order")]
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "The Display Order field needs to be greater than 0")]
         public int DisplayOrder { get; set; }
     }
 }
